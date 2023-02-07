19376ad6309b17f742044509bbeaacedd99de97e
naveenvarma600 <naveenvarma600@gmail.com>
2023-02-07 13:46:42 +0530

    removed extra code day21.cpp

--- CPP/D21/d21p4.cpp
+++ CPP/D21/d21p4.cpp
@@ -23,40 +23,7 @@
 
 
 
-#include <bits/stdc++.h>
 
-using namespace std;
-
-int main() {
-    string s;
-    cin >> s;
-    int cnt = 0;
-    int ans = 0;
-    for (int i = 0; i < s.length(); i++) {
-        char ch = s[i];
-        if (ch == '[') {
-            cnt += 1;
-        } else {
-            if (i + 1 == s.length() || s[i + 1] == '[') {
-                if (cnt > 0) {
-                    cnt--;
-                    ans++;
-                } else {
-                    ans += 2;
-                }
-            } else {
-                i++;
-                if (cnt > 0) {
-                    cnt--;
-                } else {
-                    ans += 1;
-                }
-            }
-        }
-    }
-    cout << ans + cnt * 2 << endl;
-    return 0;
-}
 
 // Sample Input-1:
 // ---------------
